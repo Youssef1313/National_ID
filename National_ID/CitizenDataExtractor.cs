603b0ad591f0a02dccc45b90cb6e44dc14b91a4f
Youssef Victor <youssefvictor00@gmail.com>
2019-06-08 12:52:08 +0200

    Update thrown exceptions

--- National_ID/CitizenDataExtractor.cs
+++ National_ID/CitizenDataExtractor.cs
@@ -10,13 +10,20 @@ namespace National_ID
         {
             if (!Regex.IsMatch(id, "^(\\d){14}$"))
             {
-                throw new InvalidEnumArgumentException($"{nameof(id)} must be a string of 14 digits.");
+                throw new ArgumentException("National ID must be a string of 14 digits.", nameof(id));
             }
-            
+
+            var governorate = ((Governorates)Convert.ToByte(id.Substring(7, 2))).ToString();
+
+            if (governorate == "0")
+            {
+                throw new ArgumentException("National ID isn't valid");
+            }
+
             var year = 1700 + (int)(100 * char.GetNumericValue(id[0])) + Convert.ToByte(id.Substring(1, 2));
             var month = Convert.ToByte(id.Substring(3, 2));
             var day = Convert.ToByte(id.Substring(5, 2));
-            var governorate = ((Governorates) Convert.ToByte(id.Substring(7, 2))).ToString();
+
             var gender = ((byte)char.GetNumericValue(id[12]) % 2 == 1) ? "Male" : "Female";
             return new CitizenData(year, month, day, governorate, gender);
 
